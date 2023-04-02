272b2ecbc2c65e27638c191727bffdd542a771ca
estebanzt9 <estebanzapatatuberquia@gmail.com>
2023-04-02 13:40:14 -0500

    Curso Android

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val nombre = "Esteban"
+
+    if(nombre.isNotEmpty()) println("El largo de nuestra variable nombre es ${nombre.length}") else {
+        println("Error la variable está vacía")
+    }
+    
+}
